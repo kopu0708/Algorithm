d5569ccea31c00c72cb2496b0d579f9e02e65165
이정철 <ghop0822@gmail.com>
2026-07-27 19:54:46 +0900

    Update 01_sum.cs

--- 자료구조/배열과 리스트/01_sum.cs
+++ 자료구조/배열과 리스트/01_sum.cs
@@ -1,1 +1,23 @@
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int[] arr = new int[a];
+
+        string numbers = Console.ReadLine();
+        for(int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = numbers[i] - '0'; 
+        }
+
+        int sum = 0;
+        foreach (int i in arr) 
+        {
+            sum += i;
+        }
+        Console.WriteLine(sum);
+    }
+}
