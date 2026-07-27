bcc9e381d230f0a4dbdbf1cd69df68c6748a68e9
이정철 <ghop0822@gmail.com>
2026-07-27 20:14:47 +0900

    Update 02평균은 넘겠지.cs

--- 자료구조/배열과 리스트/02평균은 넘겠지.cs
+++ 자료구조/배열과 리스트/02평균은 넘겠지.cs
@@ -1,1 +1,28 @@
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[x];
+
+        string input = Console.ReadLine();
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(input.Split(" ")[i]);
+        }
+
+        double[] arr2 = new double[x];
+
+        for (int i = 0; i < arr2.Length; i++)
+        {
+            arr2[i] = ((double)arr[i] / arr.Max()) * 100;
+        }
+
+        double result = arr2.Sum() / arr2.Length;
+        Console.WriteLine(result);
+    }
+}
