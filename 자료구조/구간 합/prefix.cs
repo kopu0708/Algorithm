f14b7f7c21c3666286bc79deaab700e461cf9da2
이정철 <ghop0822@gmail.com>
2026-07-28 12:29:32 +0900

    Update prefix.cs

--- 자료구조/구간 합/prefix.cs
+++ 자료구조/구간 합/prefix.cs
@@ -4,13 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] arr = { 1, 2, 3, 4, 5 };
+        string input = Console.ReadLine();
+        int[] a = Array.ConvertAll(input.Split(' '), int.Parse); // 배열 크기, 질의 개수
 
-        int[] prefix = new int[arr.Length + 1];
+        int[] arr = new int[a[0]];
 
-        for(int i = 0; i < arr.Length; i++)
+        string arrinput = Console.ReadLine();
+        arr = Array.ConvertAll(arrinput.Split(' '), int.Parse);
+
+        int[] prefix = new int[arr.Length + 1]; // prefix sum 배열
+        for(int i = 1; i < prefix.Length; i++)
         {
-            prefix[i + 1] = prefix[i] + arr[i];
+            prefix[i] = prefix[i - 1] + arr[i - 1];
+        }
+
+        for (int i = 0; i < a[1]; i++)
+        {
+            string queryInput = Console.ReadLine();
+            int[] query = Array.ConvertAll(queryInput.Split(' '), int.Parse); // 질의 입력
+            int answer = prefix[query[1]] - prefix[query[0] - 1]; // 구간 합 계산
+
+            Console.WriteLine(answer);
         }
     }
 }
