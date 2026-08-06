c98e2b1564b9448a4b4cf49c55cc6a33767ddd04
이정철 <ghop0822@gmail.com>
2026-08-06 22:06:28 +0900

    Update remain.cs

--- 자료구조/구간 합/remain.cs
+++ 자료구조/구간 합/remain.cs
@@ -52,3 +52,46 @@ class Program
 
     }
 }
+
+----
+// 2026-08-06 
+// 다시 풀어봤다. 그 과정에서 위 코드가 prefix 배열의 0번째를 포함하지 않고 풀어 첫 인덱스를 포함하는 나눠지는 합들은 따로 새어줬는데 
+// 아래 코드와 같이짜면 상당히 이해하기 편해지고 코드가 짧아졌다.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+        int N = input[0];
+        int K = input[1];
+        int[] arr = new int[N];
+        arr = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+
+        long answer = 0;
+
+        int[] prefix = new int[N + 1];
+        for(int i = 0; i < N; i++)
+        {
+            prefix[i + 1] = prefix[i] + arr[i]; 
+        }
+
+        int[] freq = new int[K]; // 나눠주는 수 이상의 숫자는 안나오니깐 K까지만 만들어주고 
+        for(int i = 0; i <= N; i++)
+        {
+            freq[prefix[i] % K]++; // 각 인데스가 해당 나머지 수가 몇번 나왔는지를 나타냄
+        }
+
+        foreach (int cnt in freq)
+        {
+            answer += (long)cnt * (cnt - 1) / 2;
+        }
+
+
+
+        Console.WriteLine(answer);
+    }
+}
