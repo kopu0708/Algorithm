59aaf56a1e0053e72f79f8260131224270b4e035
이정철 <ghop0822@gmail.com>
2026-08-07 17:52:04 +0900

    Update two_sum_pointer.cs

--- 자료구조/구간 합/two_sum_pointer.cs
+++ 자료구조/구간 합/two_sum_pointer.cs
@@ -1,36 +1,61 @@
 using System;
 
-class Program
+class IngredientCollector
 {
-    static void Main(string[] args)
+    private int numOfitem;
+    private int collect;
+    private int[] ingredients;
+    private int start;
+    private int end;
+    private int answer;
+    public int NumOfitem { get { return numOfitem; } }
+    public int Collect { get { return collect; } }
+    public int[] Ingredients { get { return ingredients; } }
+    public int Answer { get { return answer; } }
+
+    public IngredientCollector(int numOfitem, int collect, int[] ingredients)
     {
-        int N = int.Parse(Console.ReadLine());
-        int M = int.Parse(Console.ReadLine());
+        this.numOfitem = numOfitem;
+        this.collect = collect;
+        this.ingredients = ingredients;
+        this.start = 0;
+        this.end = numOfitem - 1;
+        this.answer = 0;
+    }
+    
+    public int Solve()
+    {
+        Array.Sort(ingredients);
 
-        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-
-        Array.Sort(arr); // 오르차순 정렬
-
-        int start = 0;
-        int end = N - 1;
-        int answer = 0;
         while(start < end)
         {
-            if (arr[start] + arr[end] > M)
+            if (ingredients[start] + ingredients[end] > collect)
             {
                 end--;
             }
-            else if (arr[start] + arr[end] < M)
+            else if (ingredients[start] + ingredients[end] < collect)
             {
                 start++;
             }
-            else if (arr[start] + arr[end] == M)
+            else
             {
                 answer++;
                 start++; end--;
             }
         }
+        return answer;
+    }
+}
 
-        Console.WriteLine(answer);
+class Program
+{
+    static void Main()
+    {
+        int numOfitem = int.Parse(Console.ReadLine());
+        int collect = int.Parse(Console.ReadLine());
+        int[] ingredients = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+        IngredientCollector ingredientCollector = new IngredientCollector(numOfitem, collect, ingredients);
+        Console.WriteLine(ingredientCollector.Solve());
     }
 }
